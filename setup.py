16e18e2d06e3f044f36775ac277b998cda7568cb
Ali Shahpurwala <57410691+AliShahpurwala@users.noreply.github.com>
2022-12-05 13:17:37 -0500

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+		name="amethyste-api",
+		version="0.0.1",
+		description="Unofficial Python Package for the Amethyste API",
+		author="Ali Shahpurwala",
+		author_email="ali.murtaza.am@gmail.com",
+		install_requires=[]
+	)
