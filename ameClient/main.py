1dd3ceef14cd7bb1947026bb13b53d0b3e2fe032
Ali Shahpurwala <57410691+AliShahpurwala@users.noreply.github.com>
2022-12-06 20:44:11 -0500

    Trying to piece together the module

--- ameClient/main.py
+++ ameClient/main.py
@@ -1,3 +1,5 @@
+import requests
+
 class ameClient:
 	"""
 	Class to create a connection to the Amethyste API
@@ -13,10 +15,29 @@ class ameClient:
 		Description:
 			- Constructor for the ameClient class that requires a valid token string
 		"""
-		if token is None or type(token) != "str":
+		if token is None or type(token) != str:
 			raise TypeError("Token can not be None or not a str")
 		self._token = token
 		self.baseURL = "https://v1.api.amethyste.moe"
 
-	def generate(endpoint, data = {}):
-		pass
+	def generate(self, endpoint, data = {}):
+		"""
+		Args:
+			- endpoint: str
+		"""
+		if endpoint is None or type(endpoint) != str:
+			raise TypeError("Endpoint can not be None or not a str")
+		try:
+			response = requests.request(method="POST",
+							url=f"{self.baseURL}/generate/{endpoint}",
+							data=data,
+							headers={
+							"Authorization": f"Bearer {self._token}"
+							})
+			if response.ok:
+				return response.content
+			else:
+				response.raise_for_status()
+		except Exception as e:
+			raise e
+
